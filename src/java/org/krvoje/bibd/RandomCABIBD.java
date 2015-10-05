10d16070dc1f1e53807d75a6f871bfc7d79f447c
krvoje <hperadin@gmail.com>
2015-10-05 08:57:00 +0200

    Java code added.

---
+++ src/java/org/krvoje/bibd/RandomCABIBD.java
@@ -0,0 +1,90 @@
+package org.krvoje.bibd;
+
+import java.util.Random;
+
+public class RandomCABIBD {
+
+    private static final Random rnd = new java.util.Random(System.currentTimeMillis());
+
+    public static void main(String[] args) throws Exception {
+        int v,k,lambda;
+        if(args.length !=3) {
+            System.out.println("Usage: ");
+            System.out.println("java -jar ca-bibd.jar v k lambda [optSteps]");
+        }
+        else {
+            v = Integer.parseInt(args[0]);
+            k = Integer.parseInt(args[1]);
+            lambda = Integer.parseInt(args[2]);
+
+            IncidenceStructure bibd = new IncidenceStructure(v,k,lambda);
+            new RandomCABIBD().randomCaBibd(bibd);
+
+            System.out.println("An incidence matrix for the given parameters found!");
+            System.out.print(bibd);
+        }
+    }
+
+    private void randomCaBibd(IncidenceStructure is){
+        int maxChangeFactor = (is.v-1)*Math.abs(is.r-is.lambda) + is.b;
+        for(int i=0; i<is.v; i++) {
+            for (int j=0; j<is.r; j++) {
+                is.incidences[i][j]=1;
+            }
+        }
+        is.updateCache();
+
+        while(true)
+        {
+            is.generations++;
+            if(is.isBIBD()) return;
+
+            int row = rnd.nextInt(is.v);
+
+            int activeCol = rnd.nextInt(is.b);
+            while(!is.active(row, activeCol))
+                activeCol = rnd.nextInt(is.b);
+
+            int dormantCol = rnd.nextInt(is.b);
+            while(!is.dormant(row, dormantCol))
+                dormantCol = rnd.nextInt(is.b);
+
+            int cfa = changeFactor(is,row,activeCol);
+            int cfd = changeFactor(is,row,dormantCol);
+
+            int doWeChange = rnd.nextInt(maxChangeFactor);
+            if(doWeChange < cfa && doWeChange < cfd) {
+                is.flip(row,activeCol);
+                is.flip(row,dormantCol);
+            }
+        }
+    }
+
+    private static int changeFactor(IncidenceStructure is, int row, int col) {
+        int changeFactor = 0;
+        for(int otherRow=0; otherRow<is.v; otherRow++){
+            if(otherRow==row) continue;
+            int delta = is.lambda - is.rowIntersection[row][otherRow];
+            if(delta < 0) {
+                if(is.active(row,col) && is.active(otherRow, col))
+                    changeFactor -= delta;
+                else if(is.active(row,col) && is.dormant(otherRow, col))
+                    changeFactor += delta;
+            }
+            if(delta < 0) {
+                if(is.dormant(row, col) && is.active(otherRow, col))
+                    changeFactor+=delta;
+                else if(is.dormant(row, col) && is.dormant(otherRow, col))
+                    changeFactor-=delta;
+            }
+        }
+
+        int delta = is.k - is.sumInCol[col];
+        if(is.active(row,col))
+            changeFactor -= delta;
+        else if(is.dormant(row,col))
+            changeFactor += delta;
+
+        return changeFactor >= 0 ? changeFactor : 0;
+    }
+}
